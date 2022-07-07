b04df3f73455b3afb0385c5040a996a5b2fd554e
gisra <gisra@DELL5566>
2022-07-07 14:45:52 -0600

    Add GUI

---
+++ ESTRATEGIA/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace ESTRATEGIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelNumero7 = new System.Windows.Forms.Label();
+            this.labelNumero8 = new System.Windows.Forms.Label();
+            this.labelDividir = new System.Windows.Forms.Label();
+            this.labelNumero9 = new System.Windows.Forms.Label();
+            this.labelMultiplicar = new System.Windows.Forms.Label();
+            this.labelNumero6 = new System.Windows.Forms.Label();
+            this.labelNumero5 = new System.Windows.Forms.Label();
+            this.labelNumero4 = new System.Windows.Forms.Label();
+            this.labelResta = new System.Windows.Forms.Label();
+            this.labelNumero3 = new System.Windows.Forms.Label();
+            this.labelNumero2 = new System.Windows.Forms.Label();
+            this.labelNumero1 = new System.Windows.Forms.Label();
+            this.labelSuma = new System.Windows.Forms.Label();
+            this.label_Igual = new System.Windows.Forms.Label();
+            this.labelNumero0 = new System.Windows.Forms.Label();
+            this.labelPunto = new System.Windows.Forms.Label();
+            this.DisplayLabel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 90);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(0, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(330, 361);
+            this.label2.TabIndex = 2;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelNumero7
+            // 
+            this.labelNumero7.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero7.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumero7.ForeColor = System.Drawing.Color.White;
+            this.labelNumero7.Location = new System.Drawing.Point(3, 92);
+            this.labelNumero7.Name = "labelNumero7";
+            this.labelNumero7.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero7.TabIndex = 3;
+            this.labelNumero7.Text = "7";
+            this.labelNumero7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero7.Click += new System.EventHandler(this.labelNumero7_Click);
+            // 
+            // labelNumero8
+            // 
+            this.labelNumero8.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero8.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero8.ForeColor = System.Drawing.Color.White;
+            this.labelNumero8.Location = new System.Drawing.Point(79, 92);
+            this.labelNumero8.Name = "labelNumero8";
+            this.labelNumero8.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero8.TabIndex = 4;
+            this.labelNumero8.Text = "8";
+            this.labelNumero8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero8.Click += new System.EventHandler(this.labelNumero8_Click);
+            // 
+            // labelDividir
+            // 
+            this.labelDividir.BackColor = System.Drawing.Color.Gray;
+            this.labelDividir.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelDividir.ForeColor = System.Drawing.Color.White;
+            this.labelDividir.Location = new System.Drawing.Point(231, 92);
+            this.labelDividir.Name = "labelDividir";
+            this.labelDividir.Size = new System.Drawing.Size(73, 69);
+            this.labelDividir.TabIndex = 6;
+            this.labelDividir.Text = "/";
+            this.labelDividir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelDividir.Click += new System.EventHandler(this.labelDividir_Click);
+            // 
+            // labelNumero9
+            // 
+            this.labelNumero9.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero9.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero9.ForeColor = System.Drawing.Color.White;
+            this.labelNumero9.Location = new System.Drawing.Point(155, 92);
+            this.labelNumero9.Name = "labelNumero9";
+            this.labelNumero9.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero9.TabIndex = 5;
+            this.labelNumero9.Text = "9";
+            this.labelNumero9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero9.Click += new System.EventHandler(this.labelNumero9_Click);
+            // 
+            // labelMultiplicar
+            // 
+            this.labelMultiplicar.BackColor = System.Drawing.Color.Gray;
+            this.labelMultiplicar.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelMultiplicar.ForeColor = System.Drawing.Color.White;
+            this.labelMultiplicar.Location = new System.Drawing.Point(231, 164);
+            this.labelMultiplicar.Name = "labelMultiplicar";
+            this.labelMultiplicar.Size = new System.Drawing.Size(73, 69);
+            this.labelMultiplicar.TabIndex = 10;
+            this.labelMultiplicar.Text = "*";
+            this.labelMultiplicar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelMultiplicar.Click += new System.EventHandler(this.labelMultiplicar_Click);
+            // 
+            // labelNumero6
+            // 
+            this.labelNumero6.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero6.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero6.ForeColor = System.Drawing.Color.White;
+            this.labelNumero6.Location = new System.Drawing.Point(155, 164);
+            this.labelNumero6.Name = "labelNumero6";
+            this.labelNumero6.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero6.TabIndex = 9;
+            this.labelNumero6.Text = "6";
+            this.labelNumero6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero6.Click += new System.EventHandler(this.labelNumero6_Click);
+            // 
+            // labelNumero5
+            // 
+            this.labelNumero5.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero5.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero5.ForeColor = System.Drawing.Color.White;
+            this.labelNumero5.Location = new System.Drawing.Point(79, 164);
+            this.labelNumero5.Name = "labelNumero5";
+            this.labelNumero5.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero5.TabIndex = 8;
+            this.labelNumero5.Text = "5";
+            this.labelNumero5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero5.Click += new System.EventHandler(this.labelNumero5_Click);
+            // 
+            // labelNumero4
+            // 
+            this.labelNumero4.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero4.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero4.ForeColor = System.Drawing.Color.White;
+            this.labelNumero4.Location = new System.Drawing.Point(3, 164);
+            this.labelNumero4.Name = "labelNumero4";
+            this.labelNumero4.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero4.TabIndex = 7;
+            this.labelNumero4.Text = "4";
+            this.labelNumero4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero4.Click += new System.EventHandler(this.labelNumero4_Click);
+            // 
+            // labelResta
+            // 
+            this.labelResta.BackColor = System.Drawing.Color.Gray;
+            this.labelResta.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelResta.ForeColor = System.Drawing.Color.White;
+            this.labelResta.Location = new System.Drawing.Point(231, 236);
+            this.labelResta.Name = "labelResta";
+            this.labelResta.Size = new System.Drawing.Size(73, 69);
+            this.labelResta.TabIndex = 14;
+            this.labelResta.Text = "-";
+            this.labelResta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelResta.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // labelNumero3
+            // 
+            this.labelNumero3.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero3.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero3.ForeColor = System.Drawing.Color.White;
+            this.labelNumero3.Location = new System.Drawing.Point(155, 236);
+            this.labelNumero3.Name = "labelNumero3";
+            this.labelNumero3.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero3.TabIndex = 13;
+            this.labelNumero3.Text = "3";
+            this.labelNumero3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero3.Click += new System.EventHandler(this.labelNumero3_Click);
+            // 
+            // labelNumero2
+            // 
+            this.labelNumero2.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero2.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero2.ForeColor = System.Drawing.Color.White;
+            this.labelNumero2.Location = new System.Drawing.Point(79, 236);
+            this.labelNumero2.Name = "labelNumero2";
+            this.labelNumero2.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero2.TabIndex = 12;
+            this.labelNumero2.Text = "2";
+            this.labelNumero2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero2.Click += new System.EventHandler(this.labelNumero2_Click);
+            // 
+            // labelNumero1
+            // 
+            this.labelNumero1.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero1.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero1.ForeColor = System.Drawing.Color.White;
+            this.labelNumero1.Location = new System.Drawing.Point(3, 236);
+            this.labelNumero1.Name = "labelNumero1";
+            this.labelNumero1.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero1.TabIndex = 11;
+            this.labelNumero1.Text = "1";
+            this.labelNumero1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero1.Click += new System.EventHandler(this.labelNumero1_Click);
+            // 
+            // labelSuma
+            // 
+            this.labelSuma.BackColor = System.Drawing.Color.Gray;
+            this.labelSuma.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelSuma.ForeColor = System.Drawing.Color.White;
+            this.labelSuma.Location = new System.Drawing.Point(231, 308);
+            this.labelSuma.Name = "labelSuma";
+            this.labelSuma.Size = new System.Drawing.Size(73, 69);
+            this.labelSuma.TabIndex = 18;
+            this.labelSuma.Text = "+";
+            this.labelSuma.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSuma.Click += new System.EventHandler(this.labelSuma_Click);
+            // 
+            // label_Igual
+            // 
+            this.label_Igual.BackColor = System.Drawing.Color.Gray;
+            this.label_Igual.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.label_Igual.ForeColor = System.Drawing.Color.White;
+            this.label_Igual.Location = new System.Drawing.Point(155, 308);
+            this.label_Igual.Name = "label_Igual";
+            this.label_Igual.Size = new System.Drawing.Size(73, 69);
+            this.label_Igual.TabIndex = 17;
+            this.label_Igual.Text = "=";
+            this.label_Igual.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label_Igual.Click += new System.EventHandler(this.label_Igual_Click);
+            // 
+            // labelNumero0
+            // 
+            this.labelNumero0.BackColor = System.Drawing.Color.Gray;
+            this.labelNumero0.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelNumero0.ForeColor = System.Drawing.Color.White;
+            this.labelNumero0.Location = new System.Drawing.Point(79, 308);
+            this.labelNumero0.Name = "labelNumero0";
+            this.labelNumero0.Size = new System.Drawing.Size(73, 69);
+            this.labelNumero0.TabIndex = 16;
+            this.labelNumero0.Text = "0";
+            this.labelNumero0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNumero0.Click += new System.EventHandler(this.labelNumero0_Click);
+            // 
+            // labelPunto
+            // 
+            this.labelPunto.BackColor = System.Drawing.Color.Gray;
+            this.labelPunto.Font = new System.Drawing.Font("Berlin Sans FB", 26.25F);
+            this.labelPunto.ForeColor = System.Drawing.Color.White;
+            this.labelPunto.Location = new System.Drawing.Point(3, 308);
+            this.labelPunto.Name = "labelPunto";
+            this.labelPunto.Size = new System.Drawing.Size(73, 69);
+            this.labelPunto.TabIndex = 15;
+            this.labelPunto.Text = "C";
+            this.labelPunto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelPunto.Click += new System.EventHandler(this.labelPunto_Click);
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.DisplayLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DisplayLabel.Font = new System.Drawing.Font("Berlin Sans FB", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayLabel.ForeColor = System.Drawing.Color.White;
+            this.DisplayLabel.Location = new System.Drawing.Point(8, 9);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.ReadOnly = true;
+            this.DisplayLabel.Size = new System.Drawing.Size(291, 71);
+            this.DisplayLabel.TabIndex = 19;
+            this.DisplayLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 380);
+            this.Controls.Add(this.DisplayLabel);
+            this.Controls.Add(this.labelSuma);
+            this.Controls.Add(this.label_Igual);
+            this.Controls.Add(this.labelNumero0);
+            this.Controls.Add(this.labelPunto);
+            this.Controls.Add(this.labelResta);
+            this.Controls.Add(this.labelNumero3);
+            this.Controls.Add(this.labelNumero2);
+            this.Controls.Add(this.labelNumero1);
+            this.Controls.Add(this.labelMultiplicar);
+            this.Controls.Add(this.labelNumero6);
+            this.Controls.Add(this.labelNumero5);
+            this.Controls.Add(this.labelNumero4);
+            this.Controls.Add(this.labelDividir);
+            this.Controls.Add(this.labelNumero9);
+            this.Controls.Add(this.labelNumero8);
+            this.Controls.Add(this.labelNumero7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelNumero7;
+        private System.Windows.Forms.Label labelNumero8;
+        private System.Windows.Forms.Label labelDividir;
+        private System.Windows.Forms.Label labelNumero9;
+        private System.Windows.Forms.Label labelMultiplicar;
+        private System.Windows.Forms.Label labelNumero6;
+        private System.Windows.Forms.Label labelNumero5;
+        private System.Windows.Forms.Label labelNumero4;
+        private System.Windows.Forms.Label labelResta;
+        private System.Windows.Forms.Label labelNumero3;
+        private System.Windows.Forms.Label labelNumero2;
+        private System.Windows.Forms.Label labelNumero1;
+        private System.Windows.Forms.Label labelSuma;
+        private System.Windows.Forms.Label label_Igual;
+        private System.Windows.Forms.Label labelNumero0;
+        private System.Windows.Forms.Label labelPunto;
+        private System.Windows.Forms.TextBox DisplayLabel;
+    }
+}
+
---
+++ ESTRATEGIA/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESTRATEGIA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void labelNumero7_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "7";
+        }
+
+        private void labelNumero8_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "8";
+        }
+
+        private void labelNumero9_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "9";
+        }
+
+        private void labelNumero4_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "4";
+        }
+
+        private void labelNumero5_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "5";
+        }
+
+        private void labelNumero6_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "6";
+        }
+
+        private void labelNumero1_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "1";
+        }
+
+        private void labelNumero2_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "2";
+        }
+
+        private void labelNumero3_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "3";
+        }
+
+        private void labelPunto_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = "";
+        }
+
+        private void labelNumero0_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "0";
+        }
+
+        private void label_Igual_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelSuma_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "+";
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "-";
+        }
+
+        private void labelMultiplicar_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "*";
+        }
+
+        private void labelDividir_Click(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DisplayLabel.Text + "/";
+        }
+    }
+}
